ca5c5ef1970f1abb9a67c7f4c462992825a3a699
felipe <felipelascasas7@gmail.com>
2023-10-15 23:13:49 -0300

    first commit

---
+++ Academico/Controllers/CursoController.cs
@@ -0,0 +1,184 @@
+﻿using Academico.Data;
+using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academico.Controllers
+{
+    public class CursoController : Controller
+    {
+        private readonly AcademicoContext _context;
+
+        public CursoController(AcademicoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: aluno
+        public async Task<IActionResult> Index()
+        {
+            return _context.Cursos != null
+                ? View(await _context.Cursos.ToListAsync())
+                : Problem("Entity set 'AcademicoContext.Cursos'  is null.");
+        }
+
+        // GET: aluno/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Cursos == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Cursos.FirstOrDefaultAsync(m => m.CursoId == id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Disciplinas"] = await _context.CursosDisciplinas.Where(item => item.CursoId == id).ToListAsync();
+
+            return View(curso);
+        }
+
+        // GET: aluno/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult AddToDiscipline()
+        {
+            ViewData["CursoId"] = new SelectList(_context.Cursos, "CursoId", "Nome");
+            ViewData["DisciplinaId"] = new SelectList(_context.Disciplinas, "DisciplinaId", "Nome");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddToDiscipline([Bind("CursoId, DisciplinaId")] CursoDisciplina cursoDisciplina)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cursoDisciplina);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: aluno/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome, CargaHoraria")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(curso);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(curso);
+        }
+
+        // GET: curso/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cursos == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Cursos.FindAsync(id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+
+            return View(curso);
+        }
+
+        // POST: curso/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long? id, [Bind("CursoId,Nome,CargaHoraria")] Curso curso)
+        {
+            if (id != curso.CursoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(curso);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CursoExists(curso.CursoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(curso);
+        }
+
+        // GET: curso/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cursos == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Cursos.FirstOrDefaultAsync(m => m.CursoId == id);
+
+            if (curso == null)
+            {
+                return NotFound();
+            }
+
+            return View(curso);
+        }
+
+        // POST: curso/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (_context.Cursos == null)
+            {
+                return Problem("Entity set 'AcademicoContext.Cursos' is null.");
+            }
+
+            var curso = await _context.Cursos.FindAsync(id);
+            if (curso != null)
+            {
+                _context.Cursos.Remove(curso);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CursoExists(int? id)
+        {
+            return (_context.Cursos?.Any(e => e.CursoId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Academico/Models/Departamento.cs
@@ -0,0 +1,10 @@
+﻿namespace Academico.Models
+{
+    public class Departamento
+    {
+        public long? DepartamentoId { get; set; }
+        public string Nome { get; set; }
+        public long InstituicaoID { get; set; }
+        public Instituicao? Instituicao { get; set; }
+    }
+}
